44389754488937a2280d94cba2541fd322c170c6
J-odion <emmyjaff22@gmail.com>
2024-07-17 11:00:30 +0100

    login good email bad pass validate fix

--- src/controllers/auth/login.js
+++ src/controllers/auth/login.js
@@ -2,58 +2,58 @@ const User = require("../../models/user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-exports.login = async (req, res) => {
+exports.login = (req, res) => {
   const { email, password } = req.body;
 
   // Log the incoming request body
   console.log("Login request body:", req.body);
 
- 
-  try {
-    let user = await User.findOne({ email });
+  User.findOne({ email })
+    .then(user => {
+      // Log the retrieved user
+      console.log("User retrieved:", user);
 
-    // Log the retrieved user
-    console.log("User retrieved:", user);
-
-    if (!user) {
-      return res
-        .status(400)
-        .json({ msg: "Invalid credentials - User not found" });
-    }
-
-    if (!user.isVerified) {
-      return res
-        .status(400)
-        .json({ msg: "Invalid credentials - User not verified" });
-    }
-
-    // Compare the provided password with the hashed password in the database
-    const isMatch = bcrypt.compare(password, user.password);
-
-
-    if (!isMatch) {
-      return res
-        .status(400)
-        .json({ msg: "Invalid credentials - Incorrect password" });
-    }
-
-    const payload = {
-      user: {
-        id: user.id,
-      },
-    };
-
-    jwt.sign(
-      payload,
-      process.env.JWT_SECRET,
-      { expiresIn: 360000 },
-      (err, token) => {
-        if (err) throw err;
-        res.json({ token });
+      if (!user) {
+        return res.status(400).json({ msg: "Invalid credentials - User not found" });
       }
-    );
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server error");
-  }
+
+      if (!user.isVerified) {
+        return res.status(400).json({ msg: "Invalid credentials - User not verified" });
+      }
+
+      // Compare the provided password with the hashed password in the database
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          // Log the password comparison result
+          console.log("Password match result:", isMatch);
+
+          if (!isMatch) {
+            return res.status(400).json({ msg: "Invalid credentials - Incorrect password" });
+          }
+
+          const payload = {
+            user: {
+              id: user.id,
+            },
+          };
+
+          jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            { expiresIn: 360000 },
+            (err, token) => {
+              if (err) throw err;
+              res.json({ token });
+            }
+          );
+        })
+        .catch(err => {
+          console.error(err.message);
+          res.status(500).send("Server error");
+        });
+    })
+    .catch(err => {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    });
 };
